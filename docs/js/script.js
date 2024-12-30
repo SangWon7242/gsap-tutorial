68e5a1183576b2f0266bff8bb110eca031a0785c
SangWon7242 <gotkdqja@gmail.com>
2024-12-30 20:59:05 +0900

    gsap to를 사용하서 1초 간격으로 4번 직선이동하여 원래위치로

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,38 @@
 gsap.registerPlugin(ScrollTrigger);
 // gsap code here!
 
-gsap.to(".box-1, .box-2", {
-  x: 300,
-});
-
 gsap.to(
-  ".box-2",
+  ".box-1",
   {
-    x: 600,
     duration: 2,
+    x: 300,
   },
   1
+);
+
+gsap.to(
+  ".box-1",
+  {
+    duration: 2,
+    y: 300,
+  },
+  3
+);
+
+gsap.to(
+  ".box-1",
+  {
+    duration: 2,
+    x: 0,
+  },
+  5
+);
+
+gsap.to(
+  ".box-1",
+  {
+    duration: 2,
+    y: 0,
+  },
+  7
 );
